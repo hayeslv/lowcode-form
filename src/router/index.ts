dce9f4e73c996f009f047400ef706f631632b9a9
lvhaizhou <417703682@qq.com>
2022-05-27 17:00:24 +0800

    feat(router): 添加nprogress

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,9 @@
 import type { RouteRecordRaw } from "vue-router";
 import { createRouter, createWebHistory } from "vue-router";
+import NProgress from "nprogress";
+import "nprogress/nprogress.css"; // 进度条样式
+
+NProgress.configure({ showSpinner: false }); // NProgress Configuration
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -12,6 +16,15 @@ const routes: Array<RouteRecordRaw> = [
 const router = createRouter({
   history: createWebHistory(),
   routes,
+});
+
+router.beforeEach(() => {
+  NProgress.start(); // start progress bar
+  return true;
+});
+
+router.afterEach(() => {
+  NProgress.done(); // finish progress bar
 });
 
 export default router;
