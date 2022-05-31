38426d8b6feed1ef9a93e9b746f7574c9f3e9e0c
lvhaizhou <417703682@qq.com>
2022-05-31 17:27:20 +0800

    feat(center-board): 添加draggable

--- src/pages/center-board/index.tsx
+++ src/pages/center-board/index.tsx
@@ -1,48 +1,38 @@
 import { ElButton, ElForm, ElRow, ElScrollbar } from "element-plus";
-import { defineComponent, inject, onMounted, ref } from "vue";
+import { defineComponent, ref } from "vue";
 import { VideoPlay } from "@element-plus/icons-vue";
 import DraggableItem from "./DraggableItem";
+import { VueDraggableNext as Draggable } from "vue-draggable-next";
 import { formConfig } from "~/config/component";
 import "./index.scss";
-import { VisualDragEnd, VisualDragOver, VisualDragStart } from "~/utils";
+// import { VisualDragEnd, VisualDragOver, VisualDragStart } from "~/utils";
 
 export default defineComponent({
   setup() {
-    const drawingList = ref([]);
-    const containerRef = ref({} as any);
+    const drawingList = ref([] as any[]);
 
-    // const handler = {
-    //   dragstart: () => {
-    //     console.log("dragstart---");
-    //   },
-    //   dragover: () => {
-    //     console.log("drahover---");
-    //   },
-    //   dragend: () => {
-    //     console.log("dragend---");
+    drawingList.value = [
+      { name: "john", id: 1 },
+      { name: "dylan", id: 2 },
+      { name: "jean", id: 3 },
+    ];
+    // const containerRef = ref({} as any);
+
+    // const containerHandler = {
+    //   // 拖拽菜单组件，进入容器的时候，设置鼠标的可放置状态
+    //   dragenter: (e: DragEvent) => (e.dataTransfer!.dropEffect = "move"),
+    //   // 拖拽菜单组件，鼠标在容器中移动的时候，禁用默认事件
+    //   dragover: (e: DragEvent) => e.preventDefault(),
+    //   // 如果拖拽过程中，鼠标离开了容器，设置鼠标为不可放置的状态
+    //   dragleave: (e: DragEvent) => (e.dataTransfer!.dropEffect = "none"),
+    //   // 在容器中放置的时候，通过事件对象的 offsetX 和 offsetY 添加一条组件数据
+    //   drop: (e: DragEvent) => {
+    //     console.log("drop");
     //   },
     // };
-    // VisualDragStart.on(handler.dragstart);
-    // VisualDragOver.on(handler.dragover);
-    // VisualDragEnd.on(handler.dragend);
 
-    const containerHandler = {
-      // 拖拽菜单组件，进入容器的时候，设置鼠标的可放置状态
-      dragenter: (e: DragEvent) => (e.dataTransfer!.dropEffect = "move"),
-      // 拖拽菜单组件，鼠标在容器中移动的时候，禁用默认事件
-      dragover: (e: DragEvent) => e.preventDefault(),
-      // 如果拖拽过程中，鼠标离开了容器，设置鼠标为不可放置的状态
-      dragleave: (e: DragEvent) => (e.dataTransfer!.dropEffect = "none"),
-      // 在容器中放置的时候，通过事件对象的 offsetX 和 offsetY 添加一条组件数据
-      drop: (e: DragEvent) => {
-        console.log("drop");
-        // const blocks = [...dataModel.value.blocks || []];
-        // blocks.push(
-        //   createNewBlock({ component: component!, top: e.offsetY, left: e.offsetX }),
-        // );
-        // methods.updateBlocks(blocks);
-        // dragend.emit();
-      },
+    const logHandler = (event) => {
+      console.log(event);
     };
 
     return () => <div class="center-board">
@@ -51,22 +41,28 @@ export default defineComponent({
       </div>
       <ElScrollbar class="center-scrollbar">
         <ElRow class="center-board-row"
-          gutter={formConfig.gutter}
-          ref={containerRef}
+          gutter={formConfig.gutter}>
+          {/* ref={containerRef}
           {...{
             onDragenter: containerHandler.dragenter,
             onDragover: containerHandler.dragover,
             onDragleave: containerHandler.dragleave,
             onDrop: containerHandler.drop,
-          }}>
+          }}> */}
           <ElForm
             labelPosition={formConfig.labelPosition}
             disabled={formConfig.disabled}
             labelWidth={formConfig.labelWidth + "px"}
           >
-            <div class="drawing-board">
+            <Draggable class="drawing-board" list={drawingList.value} {...{ onChange: { logHandler } }}>
+              { drawingList.value.map((item) => (
+                // <DraggableItem key={index} />
+                <div key={item.name}>{item.name}</div>
+              )) }
+            </Draggable>
+            {/* <div class="drawing-board">
               { drawingList.value.map((element, index) => <DraggableItem key={index} />) }
-            </div>
+            </div> */}
             {!drawingList.value.length && <div class="empty-info">
               从左侧拖入或点选组件进行表单设计
             </div>}
