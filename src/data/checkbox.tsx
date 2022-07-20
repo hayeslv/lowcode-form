f7394edd714d7943278f065c4ad56c95675b1ee4
lvhaizhou <417703682@qq.com>
2022-07-20 09:33:31 +0800

    feat(data): 调整checkbox、radio的options动态功能

--- src/data/checkbox.tsx
+++ src/data/checkbox.tsx
@@ -1,7 +1,7 @@
 import { ElCheckbox, ElCheckboxGroup } from "element-plus";
-import type { FormNode } from "~/lowform-meta/instance/Node";
+import type { FormSelectNode } from "~/lowform-meta/instance/Node";
 import type { IBasePlatformNode } from "~/lowform-meta/type";
-import { EComponentType } from "~/lowform-meta/type";
+import { EOptionsDataType, EComponentType } from "~/lowform-meta/type";
 
 const data: IBasePlatformNode = {
   show: true,
@@ -13,14 +13,21 @@ const data: IBasePlatformNode = {
     { label: "选项一", value: "1" },
     { label: "选项二", value: "2" },
   ],
-  render: (node: FormNode) => {
-    const options = node.instance.options ?? data.options ?? [];
+  render: (node: FormSelectNode) => {
+    const instance = node.instance;
+    let options = instance.options ?? data.options ?? [];
     // 初始化defaultValue
     !node.instance.defaultValue && (node.instance.defaultValue = []);
+    if (instance.optionsDataType === EOptionsDataType.DYNAMIC) {
+      options = instance.reqOptions || [];
+    }
+
+    const optionsRender = () => options.map(v => (
+      <ElCheckbox label={v.value}>{v.label}</ElCheckbox>
+    ));
+
     return <ElCheckboxGroup style="width: 100%;" v-model={node.instance.defaultValue}>
-      {options.map(v => (
-        <ElCheckbox label={v.value}>{v.label}</ElCheckbox>
-      ))}
+      {optionsRender()}
     </ElCheckboxGroup>;
   },
 };
